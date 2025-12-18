bff2052ad25956ac4504498ae0926109a9635069
George Gussaov <gussaov04@gmail.com>
2025-12-19 00:39:37 +0300

    fix

--- 6/main.go
+++ 6/main.go
@@ -2,13 +2,14 @@ package main
 
 import "fmt"
 
+//Как я понял нужно возвращать слайс случайных чисел. Плюс длина слайса равна диапазону рандомных значений, надеюсь некритично
 func Random(cnt int) []int {
 	nums := make([]int, cnt)
 	ch := make(chan int)
 	for i := 0; i < cnt; i++ {
-		go func() {
+		go func(val int) {
 			ch <- i
-		}()
+		}(i)
 	}
 
 	for i := 0; i < cnt; i++ {
